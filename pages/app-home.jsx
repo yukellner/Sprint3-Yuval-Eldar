a37983f26cd299d2ce71595f2d6993de6f0e3fd1
Eldar Nir <loshinas@gmail.com>
2022-04-27 11:16:23 +0300

    test01

--- pages/app-home.jsx
+++ pages/app-home.jsx
@@ -7,6 +7,7 @@ export class AppHome extends React.Component{
 
         return <section>
         <h2>Welcome sprint3</h2>
+        <h2>hi from Eldar</h2>
         {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ea velit quae repudiandae possimus similique reiciendis. Tempora, odit sapiente quos, totam sunt commodi provident laudantium perferendis modi architecto fugit porro.</p> */}
         {/* <h3>Check out our <Link to="/car?ctg=bestSeller&vendor=audi">best selling cars</Link></h3> */}
     </section>
