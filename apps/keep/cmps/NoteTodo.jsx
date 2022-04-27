4b31a25faf8ee0f31a22530a2c8310e7ac2f924e
Yuval Kellner <yukellner@gmail.com>
2022-04-27 12:58:45 +0300

    keep starter

---
+++ apps/keep/cmps/NoteTodo.jsx
@@ -0,0 +1,10 @@
+
+export function NoteTodo({note}) {
+    return <section>
+
+        <h1>Todo note</h1>
+        <h2>{note.info.label}</h2>
+
+        {/* {cars.map(car => <CarPreview car={car} key={car.id} />)} */}
+    </section>
+}
---
+++ apps/keep/cmps/NoteTxt.jsx
@@ -0,0 +1,9 @@
+
+export function NoteTxt({note}) {
+    return <section>
+
+        <h1>txt note</h1>
+        <h1>{note.info.txt}</h1>
+        {/* {cars.map(car => <CarPreview car={car} key={car.id} />)} */}
+    </section>
+}
