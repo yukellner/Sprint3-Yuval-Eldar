e63440c87de42cf04c6547b4ba401e927a030ffa
Eldar Nir <loshinas@gmail.com>
2022-04-27 18:04:46 +0300

    update4

--- apps/email/cmp/email-detail.jsx
+++ apps/email/cmp/email-detail.jsx
@@ -3,7 +3,17 @@
 export class EmailDetail extends React.Component {
 
     render() {
-        
+
+        const email = this.props.email
+
+        return <secion className="email-detail">
+
+            <h3>from:  {email.from}</h3>
+            <h3>Subject: {email.subject}</h3>
+            <h3>Arrived at:{email.date} </h3>
+            <p>{email.txt}</p>
+        </secion>
+
     }
 
     
--- apps/email/cmp/email-preview.jsx
+++ apps/email/cmp/email-preview.jsx
@@ -1,4 +1,4 @@
-
+const { Link } = ReactRouterDOM
 
 export function EmailPriview(props) {
 
