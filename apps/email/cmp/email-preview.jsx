54f3fb06fb7b5b76a75146c20a7fd5c915341376
Eldar Nir <loshinas@gmail.com>
2022-04-30 18:40:16 +0300

    sync 15

--- apps/email/cmp/email-preview.jsx
+++ apps/email/cmp/email-preview.jsx
@@ -15,9 +15,9 @@ export function EmailPriview(props) {
     return <section className="email-priview">
                 <p className={starClass} onClick={ () => props.toggleStar(email.id)}>⭐</p>
                 <Link  to={`/emails/${email.id}`}  className="email-link" onClick={() => props.markRead(email.id) }>
-                    <p className={readClass} >{email.title}  </p>
-                </Link>
+                    <p className={`email-title-priview  ${readClass}`} >{email.title}  </p>
                     <p className={readClass}>{email.from}  </p>
+                </Link>
                 <p>{date}</p> 
                 {(props.folder === 'trash')? <button className="delete-button" onClick={()=> props.deleteEmail(email.id)}>Delete Email</button> :
                 <button className="unread-button" onClick={()=> props.toggleRead(email.id)}>{(email.isRead)? '✉' :' 📜'}</button>
